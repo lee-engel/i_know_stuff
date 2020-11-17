e079ab7a84da84050340d6cd79fc463b47de5afc
lee-engel <73740186+lee-engel@users.noreply.github.com>
2020-11-17 13:29:11 +0200

    wip

--- Python/sendfood/frontend/admin.py
+++ Python/sendfood/frontend/admin.py
@@ -10,11 +10,24 @@ class MenuCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(MenuItem)
 class MenuCategoryAdmin(admin.ModelAdmin):
-    fields = ('item_name', 'category', 'description', 'is_available', 'price', 'special_price')
+    fields = ('item_name', 'category', 'description', 'is_available', 'price', 'special_price',
+              'item_image_1', 'item_image_2', 'item_image_3')
     list_display = ('item_name', 'category', 'description', 'is_available', 'price', 'special_price')
 
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(UserProfile)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cdate', 'order_total', 'tip_total', 'payment_method', 'order_state')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'item', 'final_price')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cell_number', 'last_address')
+
+
 admin.site.register(Cart)
